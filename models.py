47f57e29d25a6bb0f7f3c71909ac0db44c63536a
dmbeastz <daveroymutisya2@gmail.com>
2023-12-31 14:58:54 +0300

    Almost done

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 # models.py
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, desc
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -14,6 +12,23 @@ class Customer(Base):
     last_name = Column(String)
     reviews = relationship("Review", back_populates="customer")
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self, session):
+        return max(self.reviews, key=lambda review: review.rating).restaurant
+
+    def add_review(self, session, restaurant, rating):
+        new_review = Review(customer=self, restaurant=restaurant, rating=rating)
+        self.reviews.append(new_review)
+        session.add(new_review)
+
+    def delete_reviews(self, session, restaurant):
+        reviews_to_delete = [review for review in self.reviews if review.restaurant == restaurant]
+        for review in reviews_to_delete:
+            session.delete(review)
+            self.reviews.remove(review)
+
 class Restaurant(Base):
     __tablename__ = 'restaurants'
 
@@ -21,6 +36,17 @@ class Restaurant(Base):
     name = Column(String)
     reviews = relationship("Review", back_populates="restaurant")
 
+    def all_reviews(self, session):
+        reviews = session.query(Review).filter_by(restaurant_id=self.id).all()
+        return [f"Review for {self.name} by {review.customer.full_name()}: {review.rating} stars." for review in reviews]
+
+    def average_star_rating(self, session):
+        ratings = [review.rating for review in self.reviews]
+        return sum(ratings) / len(ratings) if ratings else 0
+
+    @classmethod
+    def fanciest(cls, session):
+        return session.query(Restaurant).order_by(desc(Restaurant.price)).first()
 class Review(Base):
     __tablename__ = 'reviews'
 
@@ -30,3 +56,10 @@ class Review(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     customer = relationship("Customer", back_populates="reviews")
     restaurant = relationship("Restaurant", back_populates="reviews")
+    
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.rating} stars."
+
+    @classmethod
+    def all(cls, session):
+        return session.query(Review).all()
--- seeds.py
+++ seeds.py
@@ -9,7 +9,7 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Creating instances
+
 customer1 = Customer(first_name="John", last_name="Doe")
 restaurant1 = Restaurant(name="Good Eats")
 review1 = Review(customer=customer1, restaurant=restaurant1, rating=4)
@@ -20,19 +20,21 @@ session.add(review1)
 
 session.commit()
 
-# Querying and testing
+
 all_reviews = session.query(Review).all()
 for review in all_reviews:
     print(f"Review ID: {review.id}, Customer: {review.customer.full_name()}, Restaurant: {review.restaurant.name}, Rating: {review.rating}")
 
-average_rating = restaurant1.average_star_rating()
+average_rating = restaurant1.average_star_rating(session)
 print(f"Average Star Rating for {restaurant1.name}: {average_rating}")
 
-restaurant_reviews = restaurant1.reviews()
+
+restaurant_reviews = restaurant1.reviews
 for review in restaurant_reviews:
     print(f"Review ID: {review.id}, Rating: {review.rating}")
 
-restaurant_customers = restaurant1.customers()
+
+restaurant_customers = restaurant1.customers
 for customer in restaurant_customers:
     print(f"Customer: {customer.full_name()}")
 
@@ -42,3 +44,16 @@ print(f"Found Customer: {found_customer.full_name()}")
 customers_with_given_name = Customer.find_all_by_given_name("John")
 for customer in customers_with_given_name:
     print(customer.full_name())
+
+print(customer1.full_name())
+print(customer1.favorite_restaurant().name)
+
+customer2 = Customer(first_name="Jane", last_name="Doe")
+restaurant2 = Restaurant(name="Tasty Bites")
+review2 = Review(customer=customer2, restaurant=restaurant2, rating=5)
+
+session.add(customer2)
+session.add(restaurant2)
+session.add(review2)
+
+session.commit()
